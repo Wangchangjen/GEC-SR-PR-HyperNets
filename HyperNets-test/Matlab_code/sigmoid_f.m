41b636d97371c6708d6b95b4bf28f0d03bfe5c1e
Wangchangjen <38528305+Wangchangjen@users.noreply.github.com>
2021-10-01 18:02:23 +0800

    Delete HyperNets-test/Matlab_code directory

--- HyperNets-test/Matlab_code/sigmoid_f.m
+++
@@ -1,3 +0,0 @@
-function out = sigmoid_f(input)
-out = 1./(1+exp(-1*input));
-end
--- HyperNets-test/Matlab_code/tanh_f.m
+++
@@ -1,3 +0,0 @@
-function out = tanh_f(input)
-out = (exp(input) - exp(-1.*input))./(exp(input) + exp(-1.*input));
-end
